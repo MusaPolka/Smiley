77beafb84fcce3eec4fa4785f10428ded6f167bc
MusaPolka <75001112+MusaPolka@users.noreply.github.com>
2021-02-18 22:10:51 +0600

    Add files via upload

---
+++ Smiley/JS.js
@@ -0,0 +1,20 @@
+function smiley(){
+    let input = document.querySelector('.main-smiley')
+
+    
+    input.innerHTML = '&#128542;';
+}
+
+function smiley2(){
+    let input = document.querySelector('.main-smiley')
+
+    
+    input.innerHTML = '&#128578;';
+}
+
+function smiley3(){
+    let input = document.querySelector('.main-smiley')
+
+    
+    input.innerHTML = '&#128528;';
+}
